55db8d2992bbb528e7ea594e9d3edf48e3d78281
Arnout Kazemier <info@3rd-Eden.com>
2018-03-03 15:05:54 +0100

    [minor] Read config

--- config.js
+++ config.js
@@ -7,7 +7,13 @@ const fs = require('fs');
 const config = require('nconf');
 config.env();
 
-// @TODO scan for config files.
+//
+// Check if a config file was placed in our root.
+//
+const root = path.join(__dirname, 'config.json');
+if (fs.existsSync(root)) {
+  config.file({ file: root });
+}
 
 //
 // Expose our config
